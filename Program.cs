17aa945d7e7a7df65ff0568535b67da14a402a22
Amr Ahmed <amrahmed09951@gmail.com>
2026-08-30 18:44:27 +0300

    adding some logic

--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
-﻿Car car1 = new Car("Ford", "Mustang", 2022, "red");
+﻿using System.Reflection.Metadata.Ecma335;
+
+Car car1 = new Car("Ford", "Mustang", 2022, "red");
 Car car2 = new Car("Chevy", "Corvette", 2021, "blue");
 car1.drive();
 car2.drive();
+car1.MakeHornSound(); 
+car1.Accelerate(20); 
+car1.ShowCurrentSpeed(); 
 System.Console.WriteLine(Car.NumberOfCars);
 Car.StartRace();
 
-class Car
+
+public abstract class Vehicle
+{
+    protected int speed = 0;
+
+    public void go()
+    {
+        System.Console.WriteLine("This Vehicle is moving !!");
+    }
+    public abstract void MakeHornSound();
+}
+class Car : Vehicle
 {
     string make;
     string model;
@@ -28,5 +44,18 @@ class Car
     public static void StartRace()
     {
         System.Console.WriteLine("the Race has begun !");
+    }
+    public void Accelerate(int amount)
+    {
+        speed += amount; 
+        Console.WriteLine($"{make} accelerated by {amount} km/h.");
+    }
+    public override void MakeHornSound()
+    {
+        System.Console.WriteLine($"The {make} goes : Beep Beep!");
+    }
+     public void ShowCurrentSpeed()
+    {
+        Console.WriteLine($"The current speed of the {model} is {speed} km/h.");
     }
 }
